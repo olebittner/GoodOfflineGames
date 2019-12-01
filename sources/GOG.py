452a2ca984abd29e44233a239f67b355af96f1a3
Ole Bittner <dev@oleb.it>
2019-12-01 16:37:41 +0200

    add copyright notice

--- sources/GOG.py
+++ sources/GOG.py
@@ -1,3 +1,20 @@
+# Modified Version: Copyright (C) 2019  Ole Bittner
+# Original Version: Copyright (C) 2015-2019  Eddie "eddie3" (https://github.com/eddie3)
+#   the original, unmodified version at the time of implementation into this project is available under:
+#   https://github.com/eddie3/gogrepo/tree/2cd3ac4a77d3e5104662fcfb4d96e7129a758bfe
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import codecs
 import contextlib
 import getpass
